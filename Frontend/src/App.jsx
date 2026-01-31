d3420ae16559c3c93902390adfed96340d024d5c
Anmolsharma650 <anmolsharma9896957756@gmail.com>
2026-01-31 19:25:14 +0530

    nav complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Products from './Pages/Products'
+import Contact from './Pages/Contact'
+import About from './Pages/About'
+import Cart from './Pages/Cart'
+import Header from './component/Header/Header'
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+  
---
+++ Frontend/src/component/Header/Header.jsx
@@ -0,0 +1,62 @@
+import { MapPin, ShoppingCartIcon } from 'lucide-react'
+import React from 'react'
+import { FaCaretDown, FaCartArrowDown } from 'react-icons/fa'
+import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
+
+function Header() {
+  const [openMenu, setOpenMenu] = useState(false)
+
+  const location = false
+  return (
+    <div className=' bg-white shadow-2xl py-3'>
+      <div className='max-w-6xl mx-auto flex  justify-between items-center'>
+        {/*logo */}
+        <div className='flex  justify-center gap-5 items-center'>
+          <Link to={"/"}>
+            <h1 className='font-bold ml-5 text-3xl'><span className='text-red-400 font-serif'>Z</span>eptro</h1>
+          </Link>  <div className='flex justify-center cursor-pointer gap-2'>
+            <MapPin className=' md:flex hidden text-red-500' />
+            <span className='   md:flex hidden font-semibold'>{location ? <div></div> : "Add Location"}</span>
+            <FaCaretDown className=' md:flex hidden justify-center items-center font-medium' />
+          </div>
+        </div>
+        {/*menu*/}
+        <div className='flex text-[20px]  items-center gap-7'>
+          <ul className='   hidden  md:flex justify-center gap-7 font-semibold '>
+            <NavLink to={'/'} className={({ isActive }) => `${isActive ? "border-b-2 translate-all border-red-500 " : 'text-black '} cursor-pointer`} ><li>Home</li></NavLink>
+            <NavLink to={'/about'} className={({ isActive }) => `${isActive ? "border-b-2 translate-all border-red-500 " : 'text-black '} cursor-pointer`} ><li>About</li></NavLink>
+            <NavLink to={'/contact'} className={({ isActive }) => `${isActive ? "border-b-2 translate-all border-red-500 " : 'text-black '} cursor-pointer`} ><li>Contact</li></NavLink>
+            <NavLink to={'/products'} className={({ isActive }) => `${isActive ? "border-b-2 translate-all border-red-500 " : 'text-black '} cursor-pointer`} ><li>Produts</li></NavLink>
+          </ul>
+          {/*cart icon*/}
+
+          <div className=" cursor-pointer flex justify-center items-center gap-5">
+            <Link to="/cart">
+              <ShoppingCartIcon className="h-7 w-7 text-gray-800 hover:text-black" />
+            </Link> 
+            {/* menu icon */}
+        <div
+          className="sm:hidden text-4xl mr-5 font-bold text-gray-700 cursor-pointer"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          &#8801;
+        </div>
+          </div>
+
+          {/*Button */}
+          <div>
+            <button className='  hidden md:flex  justify-center cursor-pointer text-black px-4 py-1 font-semibold rounded-md bg-red-500'>Sign in</button>
+          </div>
+        </div>
+
+       
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
